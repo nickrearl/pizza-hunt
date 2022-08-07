1708f846f6031aa03bd5a014fa278bccbf24e2d5
Nick Earl <nick.r.earl@gmail.com>
2022-08-06 21:10:31 -0400

    added validation and staged for deployment

--- models/Pizza.js
+++ models/Pizza.js
@@ -5,10 +5,14 @@ const dateFormat = require('../utils/dateFormat')
 const PizzaSchema = new Schema(
   {
     pizzaName: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -17,7 +21,9 @@ const PizzaSchema = new Schema(
     },
     size: {
       type: String,
-      default: 'Large'
+      default: 'Large',
+      required: true,
+      enum: ['Personal', 'Small', 'Medium','Large', 'Extra Large']
     },
     toppings: [],
     comments: [
